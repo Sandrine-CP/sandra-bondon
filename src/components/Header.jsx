d3fa0cc46f45d6d90900a0d1de1d73a29389832e
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2024-12-11 23:23:33 +0100

    refactor(Header.jsx): ajout des classes BEM - style(App.css et Header.css): ajout des styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
+import "./styles/Header.css";
 
 function Header() {
 	return (
-		<div className="Header">
-			<h2>
-				<Link to="/"> S | B</Link>
-			</h2>
-			<h2>Psychopraticienne - Coach - Hypnothérapeute</h2>
+		<div className="header">
+			<h1 className="header__logo">
+				<Link to="/" aria-label="Retour à l'accueil">
+					{" "}
+					S | B
+				</Link>
+			</h1>
+			<p className="header__tgaline">
+				Psychopraticienne - Coach - Hypnothérapeute
+			</p>
 		</div>
 	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,56 @@
-import Header from "../components/Header";
+import Plume from "../assets/plumes.jpg";
+import Flowers from "../assets/fleurs.jpg";
+import "./styles/Home.css";
 
 function Home() {
 	return (
 		<>
-			<h1>Vivez-vous</h1>
-			<p>Retrouvez votre équilibre intérieur avec mes thérapies.</p>
+			<main>
+				<section className="hero">
+					<h1 className="hero__title">Vivez-vous</h1>
+					<img className="hero__image" src={Flowers} alt="Des fleurs" />
+					<div className="hero__subtitle">
+						<h2 className="hero__subtitle-text">
+							Trouvez votre équilibre pas à pas
+						</h2>
+					</div>
+					<div className="hero__text">
+						<p className="hero__paragraph">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+							cupiditate dolore numquam voluptatem rerum fugiat modi ipsum
+							similique, quis, quasi reprehenderit assumenda est cum odit labore
+							beatae nisi eaque veritatis?
+						</p>
+					</div>
+				</section>
+				<section className="specialities">
+					<h2 className="specialities__title">Spécialités</h2>
+					<p className="specialities__text">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus
+						ducimus ratione amet molestiae atque inventore labore, dolorem rem,
+						temporibus id illo? Aperiam debitis quod quibusdam exercitationem
+						quos ut illum fugit.
+					</p>
+				</section>
+				<section className="therapies">
+					<div className="therapies__title">
+						<h2>Thérapies</h2>
+					</div>
+					<div className="therapies__text">
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+							cupiditate dolore numquam voluptatem rerum fugiat modi ipsum
+							similique, quis, quasi reprehenderit assumenda est cum odit labore
+							beatae nisi eaque veritatis?
+						</p>
+					</div>
+					<img
+						className="therapies__image"
+						src={Plume}
+						alt="Une plume légère"
+					/>
+				</section>
+			</main>
 		</>
 	);
 }
