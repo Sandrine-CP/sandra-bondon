fcbe2e2731ecb361f1b7599bd1a7ac0b5e99589f
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2025-01-01 18:34:57 +0100

    refactor: migration vers Pages Router et nettoyage des dépendances

---
+++ pages/consultations.js
@@ -0,0 +1,7 @@
+export default function Consultations() {
+	return (
+		<div className="bg-green-950 text-white min-h-screen flex items-center justify-center">
+			<h1 className="text-4xl">Test OK 🎉</h1>
+		</div>
+	);
+}
---
+++ pages/therapies.js
@@ -0,0 +1,7 @@
+export default function Therapies() {
+	return (
+		<div className="bg-blue-400 text-white min-h-screen flex items-center justify-center">
+			<h1 className="text-4xl">Test OK 🎉</h1>
+		</div>
+	);
+}
