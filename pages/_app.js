faf8ce4d2ed0f1d205d49e1f03f6a7bbca847716
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2025-01-02 16:54:12 +0100

    feat(seo): ajout des balises meta globales pour le SEO dans _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
+import Head from "next/head";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<meta
+					name="keywords"
+					content="psychopraticienne, thérapie, MOSAÏC, gestion des émotions, hypnose, PNL, Louveciennes"
+				/>
+				<meta name="author" content="Sandra Bondon" />
+			</Head>
+			<Component {...pageProps} />;
+		</>
+	);
 }
