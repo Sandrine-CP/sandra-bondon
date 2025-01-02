d43184efa123cef0c14d7c8822f95bd2e96f044c
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2025-01-02 17:22:38 +0100

    chore: ajout de la balise de vérification Google Search Console

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,10 @@ export default function App({ Component, pageProps }) {
 		<>
 			<Head>
 				<meta
+					name="google-site-verification"
+					content="xpGDYjq2PEG7WEU8_zSf_F1Xwh3ThkDvQHce-zuNHxM"
+				/>
+				<meta
 					name="keywords"
 					content="psychopraticienne, thérapie, MOSAÏC, gestion des émotions, hypnose, PNL, Louveciennes"
 				/>
