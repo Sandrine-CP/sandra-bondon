a8baa6fba5f1ccfd60f1eaee35e84aa06f6e90fa
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2025-01-07 14:25:36 +0100

    chore(dependencies): mise à jour des dépendances npm et nettoyage des fichiers inutiles

---
+++ src/components/Description.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Description() {
+	return (
+		<div className="flex justify-center my-40 text-white py-20">
+			<p className="text-[7.5vw] uppercase text-center max-w-[50vw] leading-none">
+				Toutes les ressources sont en vous.
+			</p>
+		</div>
+	);
+}
---
+++ src/data.js
@@ -0,0 +1,19 @@
+import floating1 from "../public/images/1-phobie.webp";
+import floating2 from "../public/images/2-addiction.webp";
+import floating3 from "../public/images/3-trauma.webp";
+import floating4 from "../public/images/4-burnout.webp";
+import floating5 from "../public/images/5-obsession.webp";
+import floating6 from "../public/images/6-addiction2.webp";
+import floating7 from "../public/images/7-anxiété.webp";
+import floating8 from "../public/images/8-chargeMentale.webp";
+
+export {
+	floating1,
+	floating2,
+	floating3,
+	floating4,
+	floating5,
+	floating6,
+	floating7,
+	floating8,
+};
