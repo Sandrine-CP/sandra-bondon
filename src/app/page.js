55ab1893011ef93700e98211917da4c9f05b6610
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2025-01-07 15:31:04 +0100

    chore(setup): nettoyage et configuration initiale avec polices et couleurs

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,26 @@ import ParallaxSection from "@/components/ParallaxSection";
 
 export default function Home() {
 	return (
-		<main>
-			<section className="h-screen">
-				<Gallery />
-			</section>
-			<section className="h-screen">
-				<ParallaxSection />
-			</section>
-		</main>
+		<>
+			<header>
+				<div className="bg-background min-h-screen text-primary p-4">
+					<h1 className="font-heading text-3xl">
+						Bienvenue sur le site Sandra Bondon
+					</h1>
+					<p className="font-body text-lg text-primary mt-4">
+						Découvrez les solutions proposées pour surmonter vos difficultés
+						émotionnelles et relationnelles.
+					</p>
+				</div>
+			</header>
+			<main>
+				<section className="h-screen">
+					<Gallery />
+				</section>
+				<section className="h-screen">
+					<ParallaxSection />
+				</section>
+			</main>
+		</>
 	);
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+const config = {
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#2C254D", // Titres
+				secondary: "#F2E9E4", // Fond navbar
+				accent: "#FFA500", // Boutons actifs
+				background: "#FEFFFE", // Fond principal
+				darkBackground: "#2C254D", // Fond sections alternées
+				darkText: "#FEFFFE", // Texte clair
+			},
+			fontFamily: {
+				heading: ["Helvetica", "sans-serif"], // Titres
+				body: ["Arial", "sans-serif"], // Paragraphes
+			},
+		},
+	},
+	plugins: [],
 };
+
+// Exportation propre avec variable nommée
+export default config;
