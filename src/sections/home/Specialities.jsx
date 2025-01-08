0a63602bd67108faed49ff4cf9147eceab2e9a52
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2025-01-08 17:23:22 +0100

    feat: ajout des éléments avec méthode .map, test ok avec 1 objet

--- src/sections/home/Specialities.jsx
+++ src/sections/home/Specialities.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { keyframes } from "framer-motion";
 import React from "react";
 
 const specialties = [
@@ -15,6 +16,16 @@ export default function () {
 			</p>
 			<div className="flex items-center justify-center w-28 h-28 rounded-full font-semibold text-white bg-blue">
 				TEST
+			</div>
+			<div>
+				{specialties.map((item) => (
+					<div
+						key={item.id}
+						className={`flex items-center justify-center rounded-full text-white font-semibold ${item.color} ${item.size}`}
+					>
+						{item.text}
+					</div>
+				))}
 			</div>
 		</section>
 	);
