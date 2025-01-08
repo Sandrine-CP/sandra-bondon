32163e811de0199cc349f44323cdf51a23471bf2
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2025-01-08 23:06:14 +0100

    feat: ajout d'une image et début de stylisation des éléments

--- src/sections/home/Professionals.jsx
+++ src/sections/home/Professionals.jsx
@@ -1,17 +1,34 @@
+import Image from "next/image";
+import Office from "../../../public/images/professionals.jpg";
+
 export default function Professionals() {
 	return (
-		<section>
+		<section className="bg-slate-300">
 			<div>
-				<h2 className="text-2xl font-bold mb-4 text-center">
+				<h2 className="text-2xl font-bold mb-4 text-center p-4">
 					Solution pour vos collaborateurs
 				</h2>
-				<h3 className="font-semibold text-center">Accompagner</h3>
-				<p className="max-w-2xl mx-auto mb-8">
-					Nous identifions ensemble vos besoins. Nous pouvons travailler sur la
-					gestion du changement, la charge mentale, la prévention du burn-out,
-					le développement de carrière ou tout autre sujet qui permettra à vos
-					collaborateurs de vivre pleinement et sereinement leur carrière.
-				</p>
+				<div>
+					<h3 className="font-semibold text-center">Accompagner</h3>
+					<div className="flex items-center">
+						<p className="max-w-2xl mx-auto mb-8">
+							Nous identifions ensemble vos besoins. <br />
+							Nous pouvons travailler sur la gestion du changement, la charge
+							mentale, la prévention du burn-out, le développement de carrière
+							ou tout autre sujet qui permettra à vos collaborateurs de vivre
+							pleinement et sereinement leur carrière.
+						</p>
+						<div>
+							<Image
+								src={Office}
+								alt="Personnes travaillant dans un bureau"
+								width={400}
+								height={300}
+								className="rounded-lg shadow-lg"
+							/>
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
