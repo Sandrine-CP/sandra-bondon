02878dffad7ccd8b301725fc010d98876c25b123
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2025-01-08 22:41:43 +0100

    style: correction d'une classe

--- src/sections/home/Specialities.jsx
+++ src/sections/home/Specialities.jsx
@@ -33,7 +33,7 @@ export default function () {
 			<div className="mt-20">
 				<button
 					type="button"
-					class="flex justify-center gap-2 items-center mx-auto shadow-xl text-lg bg-secondary backdrop-blur-md lg:font-semibold isolation-auto border-secondary before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-primary hover:text-secondary before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group"
+					className="flex justify-center gap-2 items-center mx-auto shadow-xl text-lg bg-secondary backdrop-blur-md lg:font-semibold isolation-auto border-secondary before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-primary hover:text-secondary before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group"
 				>
 					Me contacter
 					{/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
