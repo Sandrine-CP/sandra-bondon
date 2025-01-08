204f92f9061e028f7b4ea918297b59a8015234d7
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2025-01-08 17:31:37 +0100

    feat: ajout des autres éléments du tableau

--- src/sections/home/Specialities.jsx
+++ src/sections/home/Specialities.jsx
@@ -4,7 +4,12 @@ import React from "react";
 
 const specialties = [
 	{ id: 1, text: "Addiction", color: "bg-orange", size: "w-28 h-28" },
-	{ id: 2, text: "Burn Out", color: "bg-orange", size: "w-20 h-20" },
+	{ id: 2, text: "Burn Out", color: "bg-blue", size: "w-20 h-20" },
+	{ id: 3, text: "Trauma", color: "bg-orange", size: "w-32 h-32" },
+	{ id: 4, text: "Anxiété", color: "bg-blue", size: "w-28 h-28" },
+	{ id: 5, text: "Charge Mentale", color: "bg-orange", size: "w-28 h-28" },
+	{ id: 6, text: "Obsession", color: "bg-blue", size: "w-32 h-32" },
+	{ id: 7, text: "Phobie", color: "bg-orange", size: "w-24 h-24" },
 ];
 
 export default function () {
@@ -15,7 +20,7 @@ export default function () {
 				Lorem ipsum dolor sit amet consectetur. Facilisis donec faucibus sit
 				volutpat netus quis sagittis diam sem.
 			</p>
-			<div className="flex flex-wrap justify-center gap-6">
+			<div className="relative flex flex-wrap justify-center gap-6">
 				{specialties.map((item) => (
 					<div
 						key={item.id}
