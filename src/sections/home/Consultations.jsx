aa4e573bb9bab8d41dcb9bc7007db450dc4a32a8
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2025-01-09 10:43:42 +0100

    style: changement de disposition

--- src/sections/home/Consultations.jsx
+++ src/sections/home/Consultations.jsx
@@ -1,32 +1,38 @@
 export default function Consultations() {
 	return (
 		<section className="py-16 px-4">
-			<h2 className="text-2xl font-bold mb-4 text-center p-4">Consultations</h2>
-			<p className="max-w-2xl mx-auto mb-8 text-center font-semibold p-4">
+			<h2 className="text-2xl font-bold mb-4 text-center">Consultations</h2>
+			<p className="max-w-2xl mx-auto mb-4 text-center font-semibold">
 				Horaires : 9h00 - 20h00
 			</p>
-			<p className="max-w-2xl mx-auto mb-8 text-center p-4">Modalités</p>
-			<div className="flex justify-around items-center">
-				<div className="bg-primary text-secondary rounded-40px p-3">
-					Cabinet
+			<div className="grid grid-cols-3 gap-4 text-center">
+				{/* Bloc Cabinet */}
+				<div>
+					<div className="bg-primary text-secondary rounded-full py-2 px-4">
+						Cabinet
+					</div>
+					<p className="text-gray-500 mt-2 text-sm">3 emplacements</p>
 				</div>
-				<div className="bg-primary text-secondary rounded-40px p-3">
-					Téléphone
+				{/* Bloc Téléphone */}
+				<div>
+					<div className="bg-primary text-secondary rounded-full py-2 px-4">
+						Téléphone
+					</div>
+					<p className="text-gray-500 mt-2 text-sm">06 01 02 03 04</p>
 				</div>
-				<div className="bg-primary text-secondary rounded-40px p-3">
-					En Ligne
+				{/* Bloc En Ligne */}
+				<div>
+					<div className="bg-primary text-secondary rounded-full py-2 px-4">
+						En Ligne
+					</div>
+					<p className="text-gray-500 mt-2 text-sm">via WhatsApp</p>
 				</div>
 			</div>
-			<div className="flex justify-around items-center">
-				<div className="bg-white text-primary text-sm text-gray-500 mt-2">
-					3 emplacements
-				</div>
-				<div className="bg-white text-primary text-sm text-gray-500 mt-2">
-					06 01 02 03 04
-				</div>
-				<div className="bg-white text-primary text-sm text-gray-500 mt-2">
-					via WhatsApp
-				</div>
+			{/* Section des Tarifs */}
+			<div className="text-center mt-8">
+				<p className="text-gray-500 text-sm">Tarifs</p>
+				<p className="text-gray-500 text-sm">Prix moyen à titre indicatif</p>
+				<p className="text-gray-500 text-sm">80 à 110 euros</p>
 			</div>
 		</section>
 	);
