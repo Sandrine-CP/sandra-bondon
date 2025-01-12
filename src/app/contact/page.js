8ceeb9e605352777c9cfc8250ffaecd2bf81a99f
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2025-01-12 10:50:40 +0100

    feat: ajout de la page Contact avec titre et paragraphe

---
+++ src/app/contact/page.js
@@ -0,0 +1,19 @@
+export default function ContactPage() {
+	return (
+		<main className="min-h-screen bg-white">
+			<section className="py-16 px-4 text-center">
+				<h1 className="text-3x1 font-bold mb-4">Contactez-moi</h1>
+				<p className="text-lg mb-8">
+					Remplissez le formulaire ci-dessous ou envoyez un email à :{" "}
+					<a
+						href="mailto:s.cazenave@hotmail.fr"
+						className="text-blue-500 underline"
+					>
+						s.cazenave@shotmail.fr
+					</a>
+					.
+				</p>{" "}
+			</section>
+		</main>
+	);
+}
