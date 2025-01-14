52ea8ed078474c67bc884d8f906d62961a8814bf
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2025-01-14 16:53:47 +0100

    style: ajustement des exit pour des transitions plus fluides

--- src/components/header/Body.jsx
+++ src/components/header/Body.jsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 const variants = {
 	initial: { opacity: 0, y: 20 },
 	enter: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+	exit: { opacity: 0, y: -20, transition: { duration: 0.3 } }, // Disparaît rapidement
 };
 
 export default function Body({ links, onClose }) {
@@ -16,13 +17,14 @@ export default function Body({ links, onClose }) {
 					variants={variants}
 					initial="initial"
 					animate="enter"
+					exit="exit" // Définit l'animation de sortie
 					className="text-lg uppercase font-bold"
 				>
 					{/* Lien cliquable */}
 					<Link
 						href={link.href}
 						className="hover:text-primary transition-colors duration-300"
-						onClick={onClose} // Appel de onClose au clic
+						onClick={onClose} // Appel de onClose pour fermer la Nav
 					>
 						{link.title}
 					</Link>
--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -15,7 +15,7 @@ const links = [
 	{ title: "Contact", href: "/contact" },
 ];
 
-const transition = { duration: 1, ease: [0.76, 0, 0.24, 1] };
+const transition = { duration: 0.5, ease: [0.76, 0, 0.24, 1] };
 
 export const height = {
 	initial: { height: 0 },
@@ -47,7 +47,19 @@ export default function Nav({ onClose }) {
 				</button>
 
 				{/* Close Text */}
-				<span className="text-black text-sm uppercase">Close</span>
+				<motion.span
+					variants={{
+						initial: { opacity: 0 },
+						enter: { opacity: 1, transition: { duration: 0.3, delay: 0.2 } },
+						exit: { opacity: 0, transition: { duration: 0.2 } },
+					}}
+					initial="initial"
+					animate="enter"
+					exit="exit"
+					className="text-black text-sm uppercase"
+				>
+					Close
+				</motion.span>
 			</div>
 
 			{/* Links */}
