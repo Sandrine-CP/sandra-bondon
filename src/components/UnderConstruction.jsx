8cc66ba458e24130b6ae75b5f6744cca19947405
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2025-01-25 15:55:21 +0100

    style: changement de position du container UnderConstruction pour éviter le chevauchement avec le composant Footer

--- src/components/UnderConstruction.jsx
+++ src/components/UnderConstruction.jsx
@@ -2,7 +2,7 @@ import "../components/styles/pageConstruction.css";
 
 export default function UnderConstruction() {
 	return (
-		<section>
+		<section className="relative h-screen flex items-center justify-center">
 			<div className="container">
 				<div className="loader" />
 				<div className="loader" />
